1015e2231224adf9ba3b26ffdecca300175986c3
chunchuninn <inkinbb15@gmail.com>
2026-09-02 15:15:08 -0400

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -4,4 +4,5 @@ public_data <- read.csv("data/public_data.csv")
 
 # Your task: calculate the mean score for the public data.
 # Add your code here on your own branch.
-mean(score)
+summary(public_data)
+
